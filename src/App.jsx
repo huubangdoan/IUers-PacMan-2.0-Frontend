b3a8107d133563a15f82c95f61691c6cbfc5e9de
pchauityu <pchau.ityu@gmail.com>
2026-09-25 18:47:20 +0700

    feat(App): integrate Navbar and page routing logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home.jsx'
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import Introduction from './pages/Introduction';
+import Features from './pages/Features';
+import ContactUs from './pages/ContactUs';
 
 function App() {
+  // Biến lưu trang hiện tại, mặc định mở web là vào trang 'home'
+  const [currentPage, setCurrentPage] = useState('home');
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  )
+    <div className="app-container">
+      {/* Thanh menu luôn đứng trên cùng */}
+      <Navbar setCurrentPage={setCurrentPage} />
+
+      {/* Bấm menu nào thì hiển thị trang đó */}
+      <main className="content-container">
+        {currentPage === 'home' && <Home />}
+        {currentPage === 'about' && <AboutUs />}
+        {currentPage === 'intro' && <Introduction />}
+        {currentPage === 'features' && <Features />}
+        {currentPage === 'contact' && <ContactUs />}
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
